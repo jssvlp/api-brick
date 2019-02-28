0c0551ae0a567d43f5a23f99c1e3669ef3e5dd8a
Jossias Velazquez <jossias.vel@gmail.com>
2019-02-28 08:27:27 -0400

    Controllers for user and Service

---
+++ api-brick/Controllers/ServiciosController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api_brick.Data;
+using api_brick.Models;
+
+namespace api_brick.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiciosController : ControllerBase
+    {
+        private readonly BrickDbContext _context;
+
+        public ServiciosController(BrickDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Servicios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Servicio>>> GetServicios()
+        {
+            return await _context.Servicios.ToListAsync();
+        }
+
+        // GET: api/Servicios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Servicio>> GetServicio(int id)
+        {
+            var servicio = await _context.Servicios.FindAsync(id);
+
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return servicio;
+        }
+
+        // PUT: api/Servicios/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutServicio(int id, Servicio servicio)
+        {
+            if (id != servicio.ServicioID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(servicio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServicioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Servicios
+        [HttpPost]
+        public async Task<ActionResult<Servicio>> PostServicio(Servicio servicio)
+        {
+            _context.Servicios.Add(servicio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServicio", new { id = servicio.ServicioID }, servicio);
+        }
+
+        // DELETE: api/Servicios/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Servicio>> DeleteServicio(int id)
+        {
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            _context.Servicios.Remove(servicio);
+            await _context.SaveChangesAsync();
+
+            return servicio;
+        }
+
+        private bool ServicioExists(int id)
+        {
+            return _context.Servicios.Any(e => e.ServicioID == id);
+        }
+    }
+}
---
+++ api-brick/Controllers/UsuariosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api_brick.Data;
+using api_brick.Models;
+using CryptoHelper;
+
+namespace api_brick.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly BrickDbContext _context;
+
+        public UsuariosController(BrickDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // PUT: api/Usuarios/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuario(int id, Usuario usuario)
+        {
+            if (id != usuario.UsuarioID)
+            {
+                return BadRequest();
+            }
+
+            usuario.Contraseña = HashPassword(usuario.Contraseña);
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+            usuario.Contraseña = HashPassword(usuario.Contraseña);
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuario", new { id = usuario.UsuarioID }, usuario);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> DeleteUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        public Usuario ValidateCredentials(string correo, string password)
+        {
+            Usuario _usuario = (Usuario) _context.Usuarios.Where(x => x.CorreoUsuario == correo);
+
+            if (VerifyPassword(_usuario.Contraseña, password))
+            {
+                return _usuario;
+            }
+
+            return null;
+        }
+
+        // Method for hashing the password
+        public string HashPassword(string password)
+        {
+            return Crypto.HashPassword(password);
+        }
+
+        // Method to verify the password hash against the given password
+        public bool VerifyPassword(string hash, string password)
+        {
+            return Crypto.VerifyHashedPassword(hash, password);
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.UsuarioID == id);
+        }
+    }
+}
