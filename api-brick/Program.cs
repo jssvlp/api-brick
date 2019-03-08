b20b98b15b561ab663566543ef32bc95197137b7
Felix Daniel Licien Estevez <f.licien@concentra.com.do>
2019-03-08 00:49:48 -0400

    local

--- api-brick/Program.cs
+++ api-brick/Program.cs
@@ -20,6 +20,6 @@ namespace api_brick
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>(); 
     }
 }
