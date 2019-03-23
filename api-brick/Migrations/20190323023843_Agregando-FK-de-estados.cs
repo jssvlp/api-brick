c890886009c9ee7bc7eedd24bbaebc18e99b0a2c
pjms28 <pjms_28@hotmail.com>
2019-03-22 23:11:34 -0400

    Cambio en modelo de solicitudServicio, añadido EstadoID y su objeto de navegacion

---
+++ api-brick/Migrations/20190323023843_Agregando-FK-de-estados.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_brick.Migrations
+{
+    public partial class AgregandoFKdeestados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EstadoID",
+                table: "ServicioSolicituds",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NombreInmueble",
+                table: "Inmueble",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "UsuarioID",
+                keyValue: 1,
+                column: "FechaNacimiento",
+                value: new DateTime(2019, 3, 22, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServicioSolicituds_EstadoID",
+                table: "ServicioSolicituds",
+                column: "EstadoID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServicioSolicituds_Estado_EstadoID",
+                table: "ServicioSolicituds",
+                column: "EstadoID",
+                principalTable: "Estado",
+                principalColumn: "EstadoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServicioSolicituds_Estado_EstadoID",
+                table: "ServicioSolicituds");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServicioSolicituds_EstadoID",
+                table: "ServicioSolicituds");
+
+            migrationBuilder.DropColumn(
+                name: "EstadoID",
+                table: "ServicioSolicituds");
+
+            migrationBuilder.DropColumn(
+                name: "NombreInmueble",
+                table: "Inmueble");
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "UsuarioID",
+                keyValue: 1,
+                column: "FechaNacimiento",
+                value: new DateTime(2019, 3, 16, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
