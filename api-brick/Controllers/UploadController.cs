4ecdc197bde1599ecc09e4638929123c513743fb
pjms28 <pjms_28@hotmail.com>
2019-06-26 21:16:49 -0400

    Migraciones arreglo

--- api-brick/Controllers/UploadController.cs
+++ api-brick/Controllers/UploadController.cs
@@ -6,6 +6,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace api_brick.Controllers
 {
@@ -14,37 +15,76 @@ namespace api_brick.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
+        string pathForPictures;
+
+        public UploadController(IConfiguration configuration) {
+
+            this.pathForPictures = configuration["ApplicationSettings:PathForPictures"];
+        }
         [HttpPost, DisableRequestSizeLimit]
-        public IActionResult Upload()
+        public async Task<IActionResult> UploadAsync(string fileName)
         {
+            /* try
+             {
+                 var file = Request.Form.Files[0];
+                 var folderName = Path.Combine("Resources", "Images");
+                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                 if (file.Length > 0)
+                 {
+                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                     var fullPath = Path.Combine(pathToSave, fileName);
+                     var dbPath = Path.Combine(folderName, fileName);
+
+                     using (var stream = new FileStream(fullPath, FileMode.Create))
+                     {
+                         file.CopyTo(stream);
+                     }
+
+                     return Ok(new { dbPath });
+                 }
+                 else
+                 {
+                     return BadRequest();
+                 }
+             }
+             catch (Exception ex)
+             {
+                 return StatusCode(500, "Internal server error");
+             }*/
+
             try
             {
-                var file = Request.Form.Files[0];
-                var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
 
-                if (file.Length > 0)
+                foreach (var file2 in Request.Form.Files.ToList())
                 {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
 
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    if (!string.IsNullOrEmpty(file2?.FileName))
                     {
-                        file.CopyTo(stream);
+
+                        var dir = Path.Combine(this.pathForPictures, "Recursos/Imagenes");
+
+                        if (!Directory.Exists(dir))
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+
+                        var path = Path.Combine(dir, file2.FileName);
+                        using (var fileStream = new FileStream(path, FileMode.Create))
+                        {
+                            await file2.CopyToAsync(fileStream);
+                        }
+
                     }
 
-                    return Ok(new { dbPath });
-                }
-                else
-                {
-                    return BadRequest();
                 }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                return StatusCode(500, "Internal server error");
+
             }
+
+            return Ok();
         }
     }
 }
