a0f123ae28117485d56aa452a7781d74f65a71a9
Josias Velazquez <jossias.vel@gmail.com>
2019-12-22 01:46:41 -0400

    asdasd

--- api-brick/Controllers/UploadController.cs
+++ api-brick/Controllers/UploadController.cs
@@ -25,7 +25,7 @@ namespace api_brick.Controllers
                 var file = Request.Form.Files[0];
                 var folderName = Path.Combine("Resources", "Images");
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-
+                return Ok(pathToSave);
                 if (file.Length > 0)
                 {
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
