9519c6c72d72e6c3352ed5e04014fd5a369b3940
Josias Velazquez <jossias.vel@gmail.com>
2019-12-22 10:59:52 -0400

    asdasd

---
+++ api-brick/Controllers/FilesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_brick.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
+        private readonly IHostingEnvironment host;
+
+        public FilesController(IHostingEnvironment host)
+        {
+            this.host = host;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile filesData)
+        {
+            if (filesData == null) return BadRequest("Null File");
+            if (filesData.Length == 0)
+            {
+                return BadRequest("Empty File");
+            }
+
+            if (filesData.Length > 10 * 1024 * 1024) return BadRequest("Max file size exceeded.");
+
+            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(filesData.FileName).ToLower())) return BadRequest("Invalid file type.");
+
+            var uploadFilesPath = Path.Combine(host.WebRootPath, "uploads");
+
+            if (!Directory.Exists(uploadFilesPath))
+                Directory.CreateDirectory(uploadFilesPath);
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(filesData.FileName);
+            var filePath = Path.Combine(uploadFilesPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await filesData.CopyToAsync(stream);
+            }
+          
+            return Ok();
+        }
+
+
+    }
+}
