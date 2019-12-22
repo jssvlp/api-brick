55ac9c71fef7a0f75e1074fc0334df937ce20b7e
Josias Velazquez <jossias.vel@gmail.com>
2019-12-22 01:49:57 -0400

    asdasd

--- api-brick/Controllers/UploadController.cs
+++ api-brick/Controllers/UploadController.cs
@@ -24,8 +24,8 @@ namespace api_brick.Controllers
             {
                 var file = Request.Form.Files[0];
                 var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                return Ok(pathToSave);
+                var pathToSave = Path.Combine("/home/constructora-cluster/nodeapps/build-client/assets", folderName);
+              
                 if (file.Length > 0)
                 {
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
@@ -37,7 +37,7 @@ namespace api_brick.Controllers
                         file.CopyTo(stream);
                     }
 
-                    return Ok(new { dbPath });
+                    return Ok(new { fullPath });
                 }
                 else
                 {
