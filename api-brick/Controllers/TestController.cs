015d52ef3cf1f5c9441c1f4565d758098cb3a9ea
Josias Velazquez <jossias.vel@gmail.com>
2019-12-23 11:04:39 -0400

    testing log file

--- api-brick/Controllers/TestController.cs
+++ api-brick/Controllers/TestController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +13,23 @@ namespace api_brick.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        private IHostingEnvironment _environment;
+
+        public TestController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            string path = Path.Combine(_environment.ContentRootPath, "wwwroot"); //logfile.txt
+            path = Path.Combine(path, "uploads");
+            path = Path.Combine(path, "logfile.txt");
+            using (StreamWriter writer = System.IO.File.AppendText(path))
+            {
+                writer.WriteLine("log message");
+            }
+
             return new string[] { "value1", "value2", "value3" };
         }
     }
